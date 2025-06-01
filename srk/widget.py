ef5284015c1ba453a768cfcf008619eb3c0bbc19
ivan <pioner2024@mail.ru>
2025-06-01 18:04:50 +0300

    the final result

--- srk/widget.py
+++ srk/widget.py
@@ -2,44 +2,49 @@
 from masks import get_mask_card_number
 from masks import get_mask_account
 
-""" Создаем функцию для обработки данных карт и счетов."""
-def mask_account_card(card_number):
+""" Создаем функцию для обработки данных карт и счетов. """
 
+
+def mask_account_card(card_number: str) -> str:
     masked_number = get_mask_card_number(card_number)
     part_of_the_map = []
+
     # Отделяем слова от цифр.
     for character in card_number:
         if character.isalpha():
             part_of_the_map.append(character)
-        valid_characters = ''.join(part_of_the_map)
+
+    valid_characters = ''.join(part_of_the_map)
+
     # Анализируем, присутствует ли ключевое слово, и разделяем его пробелом.
     key = "Visa"
     if key in valid_characters:
-        result = valid_characters.replace(key,key +" ")
+        result = valid_characters.replace(key, key + " ")
         return f"{result} {masked_number}"
     else:
         return f"{valid_characters} {masked_number}"
 
 
+# Получение ввода номера карты
+card_number = str(input("Введите номер карты: "))
+print(mask_account_card(card_number))
 
-
-
-map = input("Введите номер карты: ")
-print(mask_account_card(map))
-
-score = input("Ведите номер счёта:")
-account_mask = get_mask_account(score)
+# Получение ввода номера счёта
+account_number = str(input("Введите номер счёта: "))
+account_mask = get_mask_account(account_number)
 print(account_mask)
 
+''' Возвращает строку с датой "ДД.ММ.ГГГГ". '''
 
 
-'''Возвращает строку с датой "ДД.ММ.ГГГГ" '''
-def get_date(correct):
+def get_date(correct: str) -> str:
     year = correct[2:4]
-    month = correct[5:8]
+    month = correct[5:7]
     day = correct[8:10]
+    # Получаем дату в формате "ДД.ММ.ГГГГ"
+    return f"{day}.{month}.{year}"
 
-    return f"{day}-{month}{year}"
+
 
 
 dete="2024-03-11T02:26:18.671407"
