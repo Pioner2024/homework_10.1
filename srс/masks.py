3c8ef03b4e7d9b83608711b23ce98e3542a5f8a6
ivan <pioner2024@mail.ru>
2025-07-07 21:01:50 +0300

    homework_10.1_result_2.0

--- srс/masks.py
+++ srс/masks.py
@@ -1,16 +1,17 @@
 def get_mask_card_number(card_number: str) -> str:
-    """Принимает на вход номер карты и возвращает ее маску"""
-    card_number = list(filter(str.isdigit, card_number))
-    sfsdf = ("".join(card_number))
+    """Принимает на вход номер карты и возвращает ее маску."""
+    card_number_digits = list(filter(str.isdigit, card_number))
+    card_number_str = "".join(card_number_digits)
 
-    beginning_number = sfsdf[:4]
-    middle_number = sfsdf[4:6]
-    end_number = sfsdf[-4:]
+    beginning_number = card_number_str[:4]
+    middle_number = card_number_str[4:6]
+    end_number = card_number_str[-4:]
+
     mask = f"{beginning_number} {middle_number}** **** {end_number}"
     return mask
 
 
 def get_mask_account(account_number: str) -> str:
-    """Принимает на вход номер счета и возвращает его маску"""
-    last = account_number[-4:]
-    return f"Счет **{last}"
+    """Принимает на вход номер счета и возвращает его маску."""
+    last_digits = account_number[-4:]
+    return f"Счет **{last_digits}"
